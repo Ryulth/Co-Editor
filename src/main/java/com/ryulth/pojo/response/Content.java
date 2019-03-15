b627ab00a5c3086aff0be7f387bb8aa9f6c18bb5
NE60098 <seong954t@gmail.com>
2019-03-15 17:46:16 +0900

    OT Docs source update

---
+++ src/main/java/com/ryulth/pojo/response/Content.java
@@ -0,0 +1,24 @@
+package com.ryulth.pojo.response;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.IOException;
+
+@Builder
+@Data
+public class Content {
+
+    private String sessionId;
+    private String insertString;
+    private int insertLength;
+    private int insertPos;
+    private int deleteLength;
+    private int deletePos;
+    private Long version;
+
+}
